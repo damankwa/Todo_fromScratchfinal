5d062772495f54a4ba202865723192f59e417683
David Amankwa <davidamankwa@gmail.com>
2025-02-25 14:14:21 -0500

    test file
    testing adding data to DB

--- test/launcher.ts
+++ test/launcher.ts
@@ -1,3 +1,11 @@
-import { handler } from "../src/services/hello";
+import { handler } from "../src/services/spaces/handler";
 
-handler({} as any, {} as any);
+//process.env.AWS_REGION = "ca-central-1";
+//process.env.TABLE_NAME = "SpaceTable-0ea62545d61f"
+
+handler({
+    httpMethod: 'POST',
+    body: JSON.stringify({
+        location: 'Tokyo'
+    })
+} as any, {} as any);
